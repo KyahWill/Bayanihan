609e88037ecb9bd33d4649eb4e21e75907274738
KyahWill <knownbutnotloved@gmail.com>
2021-07-02 18:01:52 +0800

    updated some of the data
    updated some of the data

--- CSV/CsvtoJson.py
+++ CSV/CsvtoJson.py
@@ -1,6 +1,7 @@
 import csv
 import json
 import geocoder
+
 from dataclasses import asdict, dataclass
 
 
@@ -29,11 +30,13 @@ class Pantry:
     active_fri:bool
     active_sat:bool
     active_sun:bool    
+    op_days:str
+    op_time:str
  
 #Opens the CSV document
 def initiate():
     pantries = []
-    with open('Community_Pantry.csv',encoding="utf8") as csv_file:
+    with open('CSV\\new_data.csv',encoding="utf8") as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         next(csv_reader)
         uniqueId = 1
@@ -52,54 +55,57 @@ def initiate():
             #i[14] = FB Post
             #i[15] = Remarks
         for i in csv_reader:
-            long = 0
+            lon = 0
             lat = 0    
-            
-
-            #This segment attempts geo-coding
-            address = i[6] + ", " + i[7] + ", " + i[8]
-            print(address)
-            try:
-                g = geocoder.osm(address)
-                if type(g.osm['x']) == float and type(g.osm['y']) == float:
-                    long = g.osm['x']
+            address = ""
+            if i[1] == "CPPH ORG":
+                for j in range(3):
+                    if i[j+5] != ' ' and i[j+5] != None:
+                        address+="{},".format(i[j+5])
+                #This segment attempts geo-coding
+                print('>>>>>'+address)
+                try:
+                    g = geocoder.osm(address)
+                    lon = g.osm['x']
                     lat = g.osm['y']
-                else:
-                    print(str(g.osm['x']) + " , " + str(g.osm['y']))
-            except Exception as e:
-                print(e)
-
-            pantry =Pantry(
-                    id= uniqueId,
-                    status=i[2],
-                    name=i[3],
-                    longtitude=long,
-                    latitude=lat,
-                    houseNo= i[4],
-                    street = i[5],
-                    barangay=i[6],
-                    city=i[7],
-                    province=i[8],
-                    region= i[9],
-                    gmap= i[10],
-                    fb_post=i[14],
-                    date_started=i[11],
-                    active_mon=True,
-                    active_tue=True,
-                    active_wed=True,
-                    active_thu=True,
-                    active_fri=True,
-                    active_sat=True,
-                    active_sun=True
-                )
-            print(str(pantry.id) +", "+str(pantry.name), " = ("+ str(pantry.longtitude) + " , " + str(pantry.latitude)+")" )
-            pantries.append(asdict(pantry))
-            uniqueId+=1
+                    print(str(g.osm["addr:country"]))
+                    if(g.osm["addr:country"]=='Mindanao'):
+                        pantry =Pantry(
+                        id= uniqueId,
+                        status=i[1],
+                        name=i[2],
+                        longtitude=lon,
+                        latitude=lat,
+                        houseNo= i[3],
+                        street = i[4],
+                        barangay=i[5],
+                        city=i[6],
+                        province=i[7],
+                        region= i[8],
+                        gmap= i[9],
+                        fb_post=i[13],
+                        date_started=i[10],
+                        active_mon=True,
+                        active_tue=True,
+                        active_wed=True,
+                        active_thu=True,
+                        active_fri=True,
+                        active_sat=True,
+                        active_sun=True,
+                        op_days=i[11],
+                        op_time=i[12]
+                        )
+                        print(str(pantry.id) +", "+str(pantry.name), " = ("+ str(pantry.longtitude) + " , " + str(pantry.latitude)+")" )
+                        pantries.append(asdict(pantry))
+                        uniqueId+=1
+                except Exception as e:
+                    print(e)
     return pantries
 #Scans the operational days
 
 if __name__ == "__main__":
     pantry_list = initiate()
+    print(pantry_list)
     with open("latestPantries.json", "w") as outfile: 
         json.dump(pantry_list, outfile)
     print("Done!!!!")
