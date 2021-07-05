d34cf3624093f8197bec4ea379866c21af509f47
KyahWill <knownbutnotloved@gmail.com>
2021-07-05 14:00:18 +0800

    Update PantryNameSearch.py

--- PantryNameSearch.py
+++ PantryNameSearch.py
@@ -14,10 +14,24 @@ def Search(input_string, pantry_list):
     return output
 
 def Filter(input_dictionary, pantry_list):
+
     output = []
+    #append every community pantry to output if it has equal value to input_dictionary
+    for i in pantry_list:
+        if i["region"].upper() == PantryRegionFilter.upper():
+            print("Region match found!")
+            print(i["region"])
+        else:
+            print("No region match found. Were you looking for the community pantry in...")
+            print(i["region"])
 
-
-    for pantry in pantry_list:
+        # Need to find a way to implement the same checking procedure for pantry name to cities
+        if i["city"].upper() == PantryCityFilter.upper():
+            print("City match found!")
+            print(i["city"])
+        else:
+            print("No city match found. Were you looking for the community pantry in...")
+            print(i["city"])
         pass
 
     return output
@@ -30,27 +44,21 @@ if __name__ == "__main__":
     f = open("JSON\\Pantries.json", 'r', encoding='utf8', errors='ignore')
     data = json.load(f)
 
-    inputPantryName = input("Enter a community pantry name: ")
-    PantryCityFilter = input("Enter a city: ")
-    PantryRegionFilter = input("Enter a region: ")
+    # inputPantryName = input("Enter a community pantry name: ")
+    # output = Search(input_string=inputPantryName, pantry_list=data)
+    # for i in output:
+    #     print(i['name'])
 
-    output = Search(input_string=inputPantryName, pantry_list=data)
+    input_dictionary = {
+        "region": "NCR",
+        "active_mon":True,
+        "active_thu":True
+    }
+
+    output = Filter(input_dictionary=input_dictionary, pantry_list=data)
     for i in output:
         print(i['name'])
-        if i["region"].upper() == PantryRegionFilter.upper():
-            print("Region match found!")
-            print(i["region"])
-        else:
-            print("No region match found. Were you looking for the community pantry in...")
-            print(i["region"])
 
-# Need to find a way to implement the same checking procedure for pantry name to cities
-        if i["city"].upper() == PantryCityFilter.upper():
-            print("City match found!")
-            print(i["city"])
-        else:
-            print("No city match found. Were you looking for the community pantry in...")
-            print(i["city"])
     f.close()
 
 
