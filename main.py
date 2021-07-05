5e9bce10ffd865150fd01e15b5542af12b96c274
adieflores13 <83394155+adieflores13@users.noreply.github.com>
2021-07-05 19:02:08 +0800

    Alternative doon sa drop down box
    Ayaw lumabas ng pisti

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from kivy.lang import builder
 from kivymd.app import MDApp
 from pantryMapView import PantryMapView
 from kivy.uix.button import Button
@@ -7,10 +8,12 @@ from kivymd.uix.menu import MDDropdownMenu
 from kivy.properties import ObjectProperty
 from kivy.core.window import Window
 from kivy.lang.builder import Builder
+from kivy.uix.screenmanager import Screen
+
+
 
 
 class MainApp(MDApp):
-
 
     def on_start(self):
         
@@ -18,5 +21,6 @@ class MainApp(MDApp):
         #Iloload na yung gps location.
         #magload ng gps location.
         pass
+    
 
 MainApp().run()
