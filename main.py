f2de0786fc1ecabba42eca75a71a41b1da4241d1
Ericka <erickamaeyusi@gmail.com>
2021-07-07 22:55:46 +0800

    dropdown
     sunday-saturday

--- main.py
+++ main.py
@@ -97,7 +97,94 @@ class MainApp(MDApp):
         self.screen.ids.drop_item2.set_item(text_item)
         self.menu2.dismiss()
 ##################################wednesday##############################
+        options = ["open", "not open", "doesnt matter"]
+        menu_items = [
+            {"viewclass": "IconListItem",
+             "icon": "git",
+             "height": dp(50),
+             "text": f"{options[i]}",
+             "on_release": lambda x=f"{options[i]}": self.set_item3(x)
+             } for i in range(3)
+        ]
+        self.menu3 = MDDropdownMenu(
+            caller=self.screen.ids.drop_item3,
+            items=menu_items,
+            position="center",
+            width_mult=4,
+        )
+        self.menu3.bind()
 
+
+    def set_item3(self, text_item):
+        self.screen.ids.drop_item3.set_item(text_item)
+        self.menu3.dismiss()
+####################################thursday#####################################
+        options = ["open", "not open", "doesnt matter"]
+        menu_items = [
+            {"viewclass": "IconListItem",
+             "icon": "git",
+             "height": dp(50),
+             "text": f"{options[i]}",
+             "on_release": lambda x=f"{options[i]}": self.set_item4(x)
+             } for i in range(3)
+        ]
+        self.menu4 = MDDropdownMenu(
+            caller=self.screen.ids.drop_item4,
+            items=menu_items,
+            position="center",
+            width_mult=4,
+        )
+        self.menu4.bind()
+
+
+    def set_item4(self, text_item):
+        self.screen.ids.drop_item4.set_item(text_item)
+        self.menu4.dismiss()
+###################################friday##########################################
+        options = ["open", "not open", "doesnt matter"]
+        menu_items = [
+            {"viewclass": "IconListItem",
+             "icon": "git",
+             "height": dp(50),
+             "text": f"{options[i]}",
+             "on_release": lambda x=f"{options[i]}": self.set_item5(x)
+             } for i in range(3)
+        ]
+        self.menu5 = MDDropdownMenu(
+            caller=self.screen.ids.drop_item1,
+            items=menu_items,
+            position="center",
+            width_mult=4,
+        )
+        self.menu5.bind()
+
+
+    def set_item5(self, text_item):
+        self.screen.ids.drop_item5.set_item(text_item)
+        self.menu5.dismiss()
+#######################################saturday#################################
+        options = ["open", "not open", "doesnt matter"]
+        menu_items = [
+            {"viewclass": "IconListItem",
+             "icon": "git",
+             "height": dp(50),
+             "text": f"{options[i]}",
+             "on_release": lambda x=f"{options[i]}": self.set_item6(x)
+             } for i in range(3)
+        ]
+        self.menu6 = MDDropdownMenu(
+            caller=self.screen.ids.drop_item6,
+            items=menu_items,
+            position="center",
+            width_mult=4,
+        )
+        self.menu6.bind()
+
+
+    def set_item6(self, text_item):
+        self.screen.ids.drop_item6.set_item(text_item)
+        self.menu6.dismiss()
+#################################################################################################
     def build(self):
         return self.screen
     
